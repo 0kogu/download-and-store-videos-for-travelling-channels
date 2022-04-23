cf5fc487b45c6e23ce5294b65bcc925653aedfe5
0kogu <nascimentojosue2002@gmail.com>
2022-04-22 20:37:39 -0400

    sent file

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from vimeo_downloader import Vimeo
+from countries import links, countries
+
+
+#loop through big list
+    #create a folder
+#loop the nested list
+    # download video
+    video_link = video_link.replace("external","video")
+    video_link = video_link.split(".sd", 1)[0] 
+    #store it
+    
